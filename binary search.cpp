9f26c952af59d15a17741544c1e98a95b20711ba
Michael Luc <michaelluc@astri.org>
2017-02-25 14:25:27 +0800

    added some comment for key points

--- binary search.cpp
+++ binary search.cpp
@@ -1,18 +1,16 @@
 
-
 int binary_search(vector<int> &v, int target) {
     int left = 0;
     int right = v.size() - 1;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
+    while (left <= right) {  // key point
+        int mid = left + (right - left) / 2;  // key point
         if (v[mid] == target) {
             return mid;
         } else if (target < v[mid]) {
-            right = mid - 1;
+            right = mid - 1;  // key point
         } else {
-            left = mid + 1;
+            left = mid + 1;  // key point
         }
     }
     return -1;
 }
-
